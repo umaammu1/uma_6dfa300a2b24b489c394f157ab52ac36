8c23be835a67e95accd4bdbc0fd1353f1b2f6b35
umaammu1 <145644708+umaammu1@users.noreply.github.com>
2023-10-13 01:42:23 -0700

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,52 @@
+class student:
+    def_init_(self,name,roll_number,cgpa):
+  self.name=Name
+  self.roll_number=roll_number
+  self.cgpa=cgpa
+def sort_student(student_list):
+  sorted_students=sorted(student_list,key=lamba student.cgpa,reverse=ture)
+  return sorted_students
+#example usage:
+student1=student("alice","s123",3.7)
+student2=student("bob","s124",3.5)
+student3=student("charlie",s125",3.8)
+    students=[student1,student2,student3]
+sorted_student=sor_student(student)
+#print the sorted list of student by cgpa in descending order for student in sorted _student:
+printf("Name:{student.name},roll number:{student.roll number},cgpa:{student.cgpa}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                 0
+  
